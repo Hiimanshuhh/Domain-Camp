d98a9a4e07fafa4fde63fdbf7b2585d30f4b7ae1
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-12-19 10:10:24 +0530

    first commit

---
+++ question_1.cpp
@@ -0,0 +1,16 @@
+// Sum of Natural Numbers up to N
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter a positive integer: ";
+    cin >> N;
+
+    // Calculate the sum using the formula
+    int sum = (N * (N + 1)) / 2;
+
+    cout << "The sum of natural numbers up to " << N << " is: " << sum << endl;
+
+    return 0;
+}
